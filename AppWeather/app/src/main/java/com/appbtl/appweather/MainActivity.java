6fa75195a6266d1fede9f9fa0ef88cf3d8dcb6ae
nam2698 <evisgazor@gmail.com>
2019-09-15 16:16:35 +0700

    upadte

--- AppWeather/app/src/main/java/com/appbtl/appweather/MainActivity.java
+++ AppWeather/app/src/main/java/com/appbtl/appweather/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     private RelativeLayout mainlayout;
     private Intent intent;
+    private ImageView imgvisibility,imgpressure,imghumidity,imgwind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         control();
+        imgvisibility.setImageResource(R.drawable.visibility);
+        imgpressure.setImageResource(R.drawable.airpress);
+        imghumidity.setImageResource(R.drawable.humidity);
+        imgwind.setImageResource(R.drawable.speed);
         intent = new Intent(MainActivity.this,ActivityDetails.class);
         mainlayout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
             @Override
@@ -31,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
     }
     private void control(){
         mainlayout = (RelativeLayout)findViewById(R.id.mainlayout);
+        imgvisibility = (ImageView)findViewById(R.id.imgvisibility);
+        imgpressure = (ImageView)findViewById(R.id.imgpressure);
+        imghumidity = (ImageView)findViewById(R.id.imghumidity);
+        imgwind = (ImageView)findViewById(R.id.imgwind);
     }
 }
